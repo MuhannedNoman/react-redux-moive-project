4f90749e0ae4455741faced2227ec4cf99afde56
Muhammed <muhammadburhan964@gmail.com>
2021-08-28 17:08:44 +0300

    Setup(core): Project setup config
    added eslint, prettier and husky, and setup the file structor

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": ["off"],
+
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+  },
+};
