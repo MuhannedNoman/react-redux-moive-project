96b36c2471a22f9d804124216f56efe9ed72758e
RawandDev <rawand.200091384037@gmail.com>
2021-08-29 09:30:02 +0300

    feat(fetch): add custom useFetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import SearchInput from "./components/SearchInput";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+  const { response, loading, setQuery, setLoading } = useFetch();
+
+  function queryHandler(q) {
+    setQuery(q);
+  }
+  function loadingHandler(l) {
+    setLoading(l);
+  }
+
+  console.log(response, loading);
+
   return (
     <div>
       <h1>Hello World</h1>
+      <SearchInput
+        queryHandler={queryHandler}
+        loadingHandler={loadingHandler}
+      />
     </div>
   );
 }
