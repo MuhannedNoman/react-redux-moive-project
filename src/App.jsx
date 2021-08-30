c75b84b4eb32296fdbdcadf8e95638cfa685d190
Zahraa <alwarda@hotmail.co.uk>
2021-08-29 17:53:43 -0700

    display home component in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import Home from './components/Home';
+
 function App() {
   return (
     <>
-     
+     <Home/>
     </>
   );
 }
