fcac41c577a25e031759fe97f4aea81666a02346
Zahraa <alwarda@hotmail.co.uk>
2021-08-29 17:55:11 -0700

    create a custom hook component and add api  to fetch data

---
+++ src/components/hooks/useMoviesFetch.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// take in the url
+const useMoviesFetch = (url) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState(null);
+
+    // Fetching Data
+    useEffect(() => {
+        try {
+          const fetchData = async () => {
+            await axios.get(url).then((movies) => {
+              setData(movies.data.results);
+              setIsLoaded(true);
+            });
+          };
+          fetchData();
+        } catch (error) {
+          console.log('🚀 ~ file: useMovieFetch.js ~ line 20 ~ useEffect ~ error', error);
+    
+        }
+      }, [url]);
+    
+    return { data, isLoaded };
+};
+
+export default useMoviesFetch;
