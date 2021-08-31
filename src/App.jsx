8a2544578a82eccf08864c37f5afb67fdf8f3f4e
Zahraa <alwarda@hotmail.co.uk>
2021-08-30 17:41:03 -0700

    remove extra stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 
 // Components
 import Home from './components/Home';
 
 function App() {
   // access dispatch function
-  const dispatch = useDispatch();
-  // useSelector returns the value of the state
-  // stucture of the state: {count: 0}
-  const count = useSelector(state => state.count);
+  // const dispatch = useDispatch();
+  // const count = useSelector(state => state.count);
 
   return (
     <>
      <Home/>
-      {/* Demo of how to access the state */}
-    
-           // next time don't push ur demo 🔫
-
-      <h1>{count}</h1>
-      <button onClick={() => dispatch({type: 'inc'})} type='submit'>Increase</button>
+   
+      {/* <button onClick={() => dispatch({type: 'inc'})} type='submit'>Increase</button> */}
     </>
   );
 }
