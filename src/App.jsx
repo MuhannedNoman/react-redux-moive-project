4cc58624afe1bb04322d1eba45d52e4d5ec4b76b
ahmad <ahmad.00079821@gmail.com>
2021-08-31 19:52:45 +0300

    build(setup):react-bootstrap/bootstrap/sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>hey</h1>
+      <h1 className="text-primary">Bootstrap checked</h1>
+      <p>Centered text with Sass(checked)</p>
+      <Button variant="primary">React Bootstrap checked</Button>
     </div>
   );
 }
