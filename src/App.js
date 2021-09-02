5d3423f6b233b71a06dcf3352d0d0faac814f023
Haider <haiderlek@gmail.com>
2021-09-02 15:06:22 +0300

    feat(App): add main routes

--- src/App.js
+++ src/App.js
@@ -9,18 +9,38 @@ import HomeContainer from "./Containers/HomeContainer/HomeContainer";
 import MoviesContainer from "./Containers/MoviesContainer/MoviesContainer";
 import SingleActorContainer from "./Containers/SingleActorContainer/SingleActorContainer";
 import SingleMovieContainer from "./Containers/SingleMovieContainer/SingleMovieContainer";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <div>
-      <ActorContainer />
-      <BookmarkContainer />
-      <FooterContainer />
+      {" "}
       <NavBarContainer />
-      <HomeContainer />
-      <MoviesContainer />
-      <SingleActorContainer />
-      <SingleMovieContainer />
+      <FooterContainer />
+      <Switch>
+        <Route path="/actors">
+          <ActorContainer />
+        </Route>
+        <Route path="/bookmarks">
+          <BookmarkContainer />
+        </Route>
+
+        <Route path="/" exact>
+          <HomeContainer />
+        </Route>
+        <Route exact path="/movies">
+          <MoviesContainer />
+        </Route>
+        <Route path="/actors/:actorId">
+          <SingleActorContainer />
+        </Route>
+        <Route path="/movies/:movieId">
+          <SingleMovieContainer />
+        </Route>
+        <Route path="*">
+          <HomeContainer />
+        </Route>
+      </Switch>
     </div>
   );
 }
