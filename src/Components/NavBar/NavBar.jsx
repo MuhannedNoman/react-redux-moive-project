64d7f1e46929ac8b025d1ae6e70526b28e4f30ab
Haider <haiderlek@gmail.com>
2021-09-03 01:25:33 +0300

    feat(NavBar): add NavBar component with route Links

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,79 @@
+import {
+  Box,
+  Link,
+  Text,
+  Flex,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link as ReachLink } from "react-router-dom";
 import "./NavBar.scss";
 
+import { allGenres } from "../../Redux/Slices/moviesFilterSlice";
+
 export default function NavBar() {
+  const genresList = useSelector((state) => state.moviesFilter.allGenres.list);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(allGenres());
+  }, []);
+
   return (
-    <div>
-      This is The Header Component it should contain navigation, Login, Search
+    <Box
+      borderBottom="#dc6208 solid 0.2rem"
+      px="10"
+      h="4rem"
+      bg="blackAlpha.800"
+      color="white"
+    >
+      <Flex h="100%" align="center" justify="space-between">
+        <Link as={ReachLink} to="/">
+          <Text
+            letterSpacing="1px"
+            as="h1"
+            fontSize="lg"
+            fontWeight="bold"
+            rounded="full"
+          >
+            TMDB
+          </Text>
+        </Link>
+        <HStack fontSize="lg" spacing="5">
+          <Link borderRadius="30px" to="/" as={ReachLink}>
+            Home
+          </Link>
+          <Link to="/about" as={ReachLink}>
+            About
+          </Link>
+          <Menu>
+            <MenuButton>Movies</MenuButton>
+            <MenuList h="15rem" overflow="auto" fontSize="md">
+              {genresList.map((genre) => (
+                <Link key={genre.id} to={`/movies/${genre.id}`} as={ReachLink}>
+                  <MenuItem color="black" key={genre.id}>
+                    {genre.name}{" "}
+                  </MenuItem>
+                </Link>
+              ))}
+            </MenuList>
+          </Menu>
+          <Link to="/actors" as={ReachLink}>
+            Actors
+          </Link>
+
+          <Link to="/bookmarks" as={ReachLink}>
+            Bookmarks
+          </Link>
+        </HStack>
+      </Flex>
+      {/* This is The Header Component it should contain navigation, Login, Search
       <ul>
         <li>Include a Home button that moves the user to the home page.</li>
         <li>
@@ -27,7 +96,7 @@ export default function NavBar() {
           In Home page show a filter based on popular, release date, top rated,
           now playing, and upcoming
         </li>
-      </ul>
-    </div>
+      </ul> */}
+    </Box>
   );
 }
