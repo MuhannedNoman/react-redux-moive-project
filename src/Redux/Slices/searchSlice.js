78b0fea98fb7f6d9750c19fb028c602472d2d236
Haider Ali <haiderlek@gmail.com>
2021-09-03 23:40:35 +0300

    feat(store):add searchSlice with searchValue and search API

---
+++ src/Redux/Slices/searchSlice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+// https://api.themoviedb.org/3/search/multi?api_key=e8fe6c13def75cda44726ea251c4fb8c&language=en-US&query=super&page=1&include_adult=false
+export const getSearchResult = createAsyncThunk(
+  "search/getSearchResult",
+  async (searchValue) => {
+    return axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=e8fe6c13def75cda44726ea251c4fb8c&language=en-US&query=${searchValue}&page=1&include_adult=false`
+      )
+      .then((response) => {
+        return response.data;
+      });
+  }
+);
+const searchSlice = createSlice({
+  name: "search",
+  initialState: {
+    searchValue: "",
+    searchResult: {
+      status: "",
+      list: [],
+    },
+  },
+  reducers: {
+    setSearchValue(state, action) {
+      state.searchValue = action.payload;
+    },
+    emptySearchResults(state) {
+      state.searchResult.list = [];
+    },
+  },
+  extraReducers: {
+    [getSearchResult.pending]: (state) => {
+      state.searchResult.status = "loading";
+    },
+    [getSearchResult.fulfilled]: (state, action) => {
+      state.searchResult.status = "success";
+      state.searchResult.list = action.payload.results;
+    },
+    [getSearchResult.rejected]: (state) => {
+      state.searchResult.status = "error";
+    },
+  },
+});
+export const { setSearchValue, emptySearchResults } = searchSlice.actions;
+export default searchSlice.reducer;
--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import actorsSlice from "../Slices/actorsSlice";
 import movies from "../Slices/moviesSlice";
 import moviesFilterSlice from "../Slices/moviesFilterSlice";
+import searchSlice from "../Slices/searchSlice";
 const store = configureStore({
   reducer: {
     movies: movies,
     actors: actorsSlice,
     moviesFilter: moviesFilterSlice,
+    search: searchSlice,
   },
 });
 export default store;
