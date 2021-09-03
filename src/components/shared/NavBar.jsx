6802f16e80514435fd69963a1d32fd5cf7845664
bnar98 <bnar.paskandy@gmail.com>
2021-09-03 02:38:21 -0700

    header & mainsection
    added header and mainsection

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Dropdown } from 'react-bootstrap';
+import { AiOutlineUser } from 'react-icons/ai';
+import LOGO from '../../images/logo.png';
 
-function NavBar() {
-	return <div />;
+function Header() {
+	return (
+		<>
+			<Navbar
+				collapseOnSelect
+				expand="lg"
+				fixed="top"
+				variant="dark"
+				className="navbar"
+			>
+				<Container className="nav">
+					<Navbar.Brand>
+						<img src={LOGO} alt="logo" className="logo-image" />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav>
+							<Nav.Link>
+								<p className="menu-item">Home</p>
+							</Nav.Link>
+							<Nav.Link>
+								<p className="menu-item">Actors</p>
+							</Nav.Link>
+							<Nav.Link>
+								<p className="menu-item">Movies</p>
+							</Nav.Link>
+							<NavDropdown
+								title={<span className="menu-item">Genre</span>}
+								id="collasible-nav-dropdown"
+							>
+								<NavDropdown.Item>
+									<p className="menu-item-dropdown">Action </p>
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+					<Navbar.Collapse
+						id="responsive-navbar-nav"
+						className="justify-content-end "
+					>
+						<Nav>
+							<Nav.Link style={{ display: 'none' }}>
+								<NavDropdown
+									title={
+										<span className="menu-item user-account">
+											<AiOutlineUser />
+										</span>
+									}
+									id="collasible-nav-dropdown"
+								>
+									<NavDropdown.Item>
+										<p className="menu-item-dropdown">BookMark </p>
+									</NavDropdown.Item>
+									<NavDropdown.Item>
+										<p className="menu-item-dropdown">My Profile </p>
+									</NavDropdown.Item>
+									<Dropdown.Divider />
+									<NavDropdown.Item>
+										<p className="menu-item-dropdown">LogOut </p>
+									</NavDropdown.Item>
+								</NavDropdown>
+							</Nav.Link>
+							<button type="submit" className="Login-btn">
+								LOGIN
+							</button>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
 
-export default NavBar;
+export default Header;
--- src/pages/HomePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function HomePage() {
-	return <div />;
-}
-
-export default HomePage;
